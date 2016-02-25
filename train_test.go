93321f502f546faf7c7c059b11f59e692f31ba17
Prateek Srivastava <f2prateek@gmail.com>
2016-02-25 02:45:09 -0700

    use testify/mock for tests

--- train_test.go
+++ train_test.go
@@ -9,29 +9,22 @@ import (
 	"github.com/bmizerany/assert"
 	"github.com/f2prateek/train"
 	"github.com/gohttp/response"
+	"github.com/stretchr/testify/mock"
 )
 
-type recorderInterceptor struct {
-	request  *http.Request
-	response *http.Response
-	err      error
-}
+// Interceptor that simply calls `chain.Proceed(chain.Request())`.
+var fallThrough = train.InterceptorFunc(func(chain train.Chain) (*http.Response, error) {
+	return chain.Proceed(chain.Request())
+})
 
-func (interceptor *recorderInterceptor) Intercept(chain train.Chain) (*http.Response, error) {
-	interceptor.request = chain.Request()
-	interceptor.response, interceptor.err = chain.Proceed(interceptor.request)
-	return interceptor.response, interceptor.err
-}
-
-func TestInterceptor(t *testing.T) {
+func TestFallThroughInterceptor(t *testing.T) {
 	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
 		response.OK(w, "Hello World!")
 	}))
 	defer ts.Close()
-	recorder := &recorderInterceptor{}
 
 	client := &http.Client{
-		Transport: train.Train(recorder),
+		Transport: train.Train(fallThrough),
 	}
 
 	resp, err := client.Get(ts.URL)
@@ -43,29 +36,40 @@ func TestInterceptor(t *testing.T) {
 		t.Error(err)
 	}
 	assert.Equal(t, "Hello World!\n", string(body))
+}
 
-	assert.Equal(t, ts.URL, recorder.request.URL.String())
-	assert.Equal(t, 200, recorder.response.StatusCode)
-	assert.Equal(t, nil, recorder.err)
+type MockInterceptor struct {
+	mock.Mock
+}
+
+func (m *MockInterceptor) Intercept(chain train.Chain) (*http.Response, error) {
+	args := m.Called(chain)
+	return args.Get(0).(*http.Response), args.Error(1)
+}
+
+func NewMockInterceptor() *MockInterceptor {
+	return new(MockInterceptor)
 }
 
 func TestInterceptorCanShortCircuit(t *testing.T) {
-	recorder1 := &recorderInterceptor{}
-	short := func(c train.Chain) (*http.Response, error) {
+	m1 := NewMockInterceptor()
+	{
 		ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
 			response.OK(w, "Hello World!")
 		}))
 		defer ts.Close()
-
-		return http.Get(ts.URL)
+		resp, err := http.Get(ts.URL)
+		m1.On("Intercept", mock.AnythingOfType("*train.interceptorChain")).Return(resp, err)
 	}
-	recorder2 := &recorderInterceptor{}
+	m2 := NewMockInterceptor()
 
 	client := &http.Client{
-		Transport: train.Train(recorder1, train.InterceptorFunc(short), recorder2),
+		Transport: train.Train(fallThrough, m1, m2),
 	}
 
 	resp, err := client.Get("https://golang.org/")
+
+	// Assert that the application sees our "shorted" response.
 	assert.Equal(t, nil, err)
 	assert.Equal(t, 200, resp.StatusCode)
 	defer resp.Body.Close()
@@ -75,15 +79,7 @@ func TestInterceptorCanShortCircuit(t *testing.T) {
 	}
 	assert.Equal(t, "Hello World!\n", string(body))
 
-	assert.Equal(t, "https://golang.org/", recorder1.request.URL.String())
-	assert.Equal(t, 200, recorder1.response.StatusCode)
-	assert.Equal(t, nil, recorder1.err)
-
-	if recorder2.request != nil {
-		t.Errorf("recorder2 should not have been invoked with a request.")
-	}
-	if recorder2.response != nil {
-		t.Errorf("recorder2 should not have received a response.")
-	}
-	assert.Equal(t, nil, recorder2.err)
+	// Assert our mocks.
+	m1.AssertExpectations(t)
+	m2.AssertExpectations(t)
 }
